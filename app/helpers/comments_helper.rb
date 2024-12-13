a972a8c06e1bf4641d6056cb3977393f7043e349
Krishna Dhulipala <krishnadhulipala@ucsb.edu>
2024-12-13 02:25:33 +0000

    Enable russian-doll caching for posts and comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,11 @@
 module CommentsHelper
+  def cache_key_for_comment(comment)
+    "comment/#{comment.id}/#{comment.updated_at.to_i}"
+  end
+
+  def cache_key_for_comments_section
+    count = @post.comments.count
+    max_updated_at = @post.comments.maximum(:updated_at)&.to_i
+    "comments-section/post-#{@post.id}-#{count}-#{max_updated_at}"
+  end
 end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,11 @@
 module GroupsHelper
+  def cache_key_for_post(post)
+    "post/#{post.id}/#{post.updated_at.to_i}"
+  end
+
+  def cache_key_for_post_section
+    count = @posts.count
+    max_updated_at = @posts.maximum(:updated_at)&.to_i
+    "posts-section/group-#{@group.id}-#{count}-#{max_updated_at}"
+  end
 end
